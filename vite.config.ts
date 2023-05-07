5d52776c96072bb527368c8d5ebfa3df25853ec2
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-05-07 15:06:09 +0300

    feat: add vercel.json file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: 'build',
-  },
-  esbuild: {
-    jsxInject: `import React from 'react';`,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
+  plugins: [react()],
 });
